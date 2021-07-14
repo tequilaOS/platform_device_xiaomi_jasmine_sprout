10ead85d6226120d002daddd2decf4f26589b31a
Clarence K <clarencekuiek@icloud.com>
2021-07-14 14:44:51 +0800

    jasmine_sprout: Update redfin fp to july

--- ssos_jasmine_sprout.mk
+++ ssos_jasmine_sprout.mk
@@ -27,8 +27,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210605.005 7349499 release-keys" \
-    BUILD_FINGERPRINT="google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210705.001 7380771 release-keys" \
+    BUILD_FINGERPRINT="google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
