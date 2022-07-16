b430a1393641251b4786383bdb04429ef3daa5a0
Clarence K <clarencekuiek@icloud.com>
2022-07-16 12:07:54 +0800

    jasmine_sprout: tequilaOS

--- extract-files.sh
+++ extract-files.sh
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2020 The LineageOS Project
-# Copyright (C) 2021 ShapeShiftOS
+# Copyright (C) 2022 tequilaOS
 #
 # SPDX-License-Identifier: Apache-2.0
 #
